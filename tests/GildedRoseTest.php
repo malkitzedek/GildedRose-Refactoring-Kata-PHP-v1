27bb176c36ed212b7cb78c0ab82bd76db7804e3c
Ivan <rusmatrix48@gmail.com>
2021-05-22 21:42:50 +0400

    upd

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -67,4 +67,88 @@ class GildedRoseTest extends TestCase
 
         $this->assertEquals(99, $item->sell_in);
     }
+
+    /**
+     * Проверяет, что после того, как срок храния прошел,
+     * значение свойства quality уменьшается в два раза быстрее
+     *
+     * TODO: Уточнить, когда считать, что срох хранения прошёл: при sell_in < 0 или при sell_in <= 0
+     *
+     */
+    public function testUpdateQuality_qualityReduce2TimesFasterIfSellinIsOver()
+    {
+        $item = new Item('Elixir', -1, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(38, $item->quality);
+    }
+
+    /**
+     * Проверяет, что качество товара «Aged Brie» увеличивается по мере уменьшения срока хранения
+     */
+    public function testUpdateQuality_AgedBrieItemQuality()
+    {
+        $item = new Item('Aged Brie', 100, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(41, $item->quality);
+    }
+
+    /**
+     * Проверяет, что качество товара «Backstage passes» увеличивается по мере уменьшения срока хранения
+     */
+    public function testUpdateQuality_BackstagePassesItemQuality()
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 100, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(41, $item->quality);
+    }
+
+    /**
+     * Проверяет, что качество товара «Backstage passes» увеличивается на 2
+     * когда до истечения срока хранения 10 или менее дней
+     */
+    public function testUpdateQuality_BackstagePassesItemQualityIfSellinLessThen11()
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(42, $item->quality);
+    }
+
+    /**
+     * Проверяет, что качество товара «Backstage passes» увеличивается на 3
+     * когда до истечения срока хранения 5 или менее дней
+     */
+    public function testUpdateQuality_BackstagePassesItemQualityIfSellinLessThen5()
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(43, $item->quality);
+    }
+
+    /**
+     * Проверяет, что качество товара «Backstage passes» падает до 0 после даты проведения концерта,
+     * то есть когда значение свойства sell_in становится равно 0
+     */
+    public function testUpdateQuality_BackstagePassesItemQualityIfSellinIsOver()
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', -1, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(0, $item->quality);
+    }
+
+    /**
+     * Проверяет, что «Conjured» товары теряют качество в два раза быстрее, чем обычные товары
+     */
+    public function testUpdateQuality_ConjuredItemQuality()
+    {
+        $item = new Item('Conjured Mana Cake', 100, 40);
+        (new GildedRose([$item]))->updateQuality();
+
+        $this->assertEquals(38, $item->quality);
+    }
 }
